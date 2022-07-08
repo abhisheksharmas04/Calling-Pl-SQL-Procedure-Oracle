45c01312dbfae6aa760333deb8ef0cc310ebd112
DeLL <DeLL@Abhishek>
2022-07-08 07:53:34 +0530

    Store Procedure added

--- src/main/java/com/example/demo/runner/ApplicationRunner.java
+++ src/main/java/com/example/demo/runner/ApplicationRunner.java
@@ -1,8 +1,14 @@
 package com.example.demo.runner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import com.example.demo.service.IVaccineManagementService;
+
 public class ApplicationRunner implements CommandLineRunner{
+	
+	@Autowired
+	private IVaccineManagementService service;
 
 	@Override
 	public void run(String... args) throws Exception {
