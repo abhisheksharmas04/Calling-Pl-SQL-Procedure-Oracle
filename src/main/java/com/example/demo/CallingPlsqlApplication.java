2e85fa0e6d2dd5c9d9664715f6682c0f2ecd0af6
DeLL <DeLL@Abhishek>
2022-07-08 07:27:17 +0530

    Project Created for Calling Oracle PL/SQL Procedures

---
+++ src/main/java/com/example/demo/CallingPlsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingPlsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingPlsqlApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/runner/ApplicationRunner.java
@@ -0,0 +1,12 @@
+package com.example.demo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class ApplicationRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
