9c58377184b45dec16afaffd5fb90118d041b09f
DeLL <DeLL@Abhishek>
2022-07-11 07:46:46 +0530

    functions and procedures

--- src/main/java/com/example/demo/runner/ApplicationRunner.java
+++ src/main/java/com/example/demo/runner/ApplicationRunner.java
@@ -12,7 +12,12 @@ public class ApplicationRunner implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		//System.out.println(service.authenticate("raja", "rani"));
+		Object result[] = service.getEmpDetailsByEno(101);
+		System.out.println("Emp Name: " + result[1]);
+		System.out.println("Emp Desg: " + result[2]);
+		System.out.println("Emp Salary: " + result[3]);
+		System.out.println("Emp Dept: " + result[4]);
 	}
 
 }
--- src/main/java/com/example/demo/service/IVaccineManagementService.java
+++ src/main/java/com/example/demo/service/IVaccineManagementService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
 public interface IVaccineManagementService {
+	public String authenticate(String user, String password);
+	public Object[] getEmpDetailsByEno(int eno);
 
 }
